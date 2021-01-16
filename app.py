4e4835416ee0c6e4f0d19d27a484aa355ae4c340
root <ibohuk@mail.ru>
2021-01-16 08:45:56 -0500

    add run file for Heroku

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(StoreList, '/stores')
+
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
